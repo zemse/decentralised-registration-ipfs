49618e6ef86746e9a3c0ba805b6fe0e290d2e966
zemse <sohamzemse143@gmail.com>
2020-03-30 23:50:57 +0530

    fix export bug

---
+++ precache-manifest.1c32cee8e4a949d8b51e60dabae937e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7023dc6ba88fbdce92b72a04a54d972",
+    "url": "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/index.html"
+  },
+  {
+    "revision": "06388f4c5c108237f2b9",
+    "url": "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/static/css/main.63f7d825.chunk.css"
+  },
+  {
+    "revision": "48843eaefd1ba99daa1c",
+    "url": "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/static/js/2.2917c152.chunk.js"
+  },
+  {
+    "revision": "78d7a16074ae49492667f04783c80fc6",
+    "url": "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/static/js/2.2917c152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06388f4c5c108237f2b9",
+    "url": "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/static/js/main.ec501aa7.chunk.js"
+  },
+  {
+    "revision": "c5f0fa2aca6ff3d945c8",
+    "url": "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/static/js/runtime-main.6d4574a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/decentralised-registration-ipfs/precache-manifest.3c5f1c887e343882cf923d628be29723.js"
+  "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/precache-manifest.1c32cee8e4a949d8b51e60dabae937e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/decentralised-registration-ipfs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
