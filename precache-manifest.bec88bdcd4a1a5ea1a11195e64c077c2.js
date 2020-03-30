7367fab042cf9f4965b06f912f0bc60c14057eda
zemse <sohamzemse143@gmail.com>
2020-03-30 23:55:35 +0530

    fix routing

---
+++ precache-manifest.bec88bdcd4a1a5ea1a11195e64c077c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9297ffc2d44e5c209ef56a20bbc5dd",
+    "url": "/decentralised-registration-ipfs/index.html"
+  },
+  {
+    "revision": "acc21ce190918fd65467",
+    "url": "/decentralised-registration-ipfs/static/css/main.63f7d825.chunk.css"
+  },
+  {
+    "revision": "dc4f77da0dce71d2a5ef",
+    "url": "/decentralised-registration-ipfs/static/js/2.1aa6712e.chunk.js"
+  },
+  {
+    "revision": "78d7a16074ae49492667f04783c80fc6",
+    "url": "/decentralised-registration-ipfs/static/js/2.1aa6712e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc21ce190918fd65467",
+    "url": "/decentralised-registration-ipfs/static/js/main.f8e34d66.chunk.js"
+  },
+  {
+    "revision": "51db7a6cf51cea21351b",
+    "url": "/decentralised-registration-ipfs/static/js/runtime-main.4e12e180.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/decentralised-registration-ipfs/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/precache-manifest.1c32cee8e4a949d8b51e60dabae937e4.js"
+  "/decentralised-registration-ipfs/precache-manifest.bec88bdcd4a1a5ea1a11195e64c077c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Users/sohamzemse/soham/workspace/blockchain/ethereum/d-reg/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/decentralised-registration-ipfs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
